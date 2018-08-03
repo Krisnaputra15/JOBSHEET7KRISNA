5db8941cc80d4463b076b4a5ba118fede5dd772a
ASUS <ASUS@LAPTOP-2LC0MCD5>
2018-08-03 13:12:21 +0700

    Tambah Input Bahan 2.

--- src/SaranBahan2Resep/SaranBahan2Resep.java
+++ src/SaranBahan2Resep/SaranBahan2Resep.java
@@ -25,6 +25,31 @@ public class SaranBahan2Resep {
        System.out.print("Masukkan no pilihan anda : ");
        int bahan1 = scanner.nextInt();
        
+       if (bahan1 == 1 || bahan1 == 2){
+           System.out.println("1. susu");
+           System.out.println("2. Minyak goreng");
+           System.out.println("3. Tidak ada");
+       }
+       else {
+           System.out.println("1. Minyak goreng");
+           System.out.println("2. Roti");
+           System.out.println("3. Tidak ada");}
+           
+           System.out.print("Masukkan no Pilihan anda : ");
+           int bahan2=scanner.nextInt();
+           
+    }else
+       System.out.println("Mohon Maaf, pilihan tidak ditemukan, " 
+               + "tidak dapat memberikan bahan resep"); 
+
+    
+    
+           
+           
+                   
+       }
+              
+       
        
         
         
