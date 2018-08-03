3b0dc468b5bf884a30b6897da9761c45efa499a0
ASUS <ASUS@LAPTOP-2LC0MCD5>
2018-08-03 11:26:13 +0700

    Coba IF..ELSE

--- src/CobaPercabangan/CobaPercabangan.java
+++ src/CobaPercabangan/CobaPercabangan.java
@@ -22,7 +22,7 @@ public class CobaPercabangan {
         
         Scanner scanner = new Scanner(System.in);
         // TODO code application logic here
-        System.out.print("Nila a ? ");
+        System.out.print("Nilai a ? ");
         int a = scanner.nextInt();
         
         System.out.println("\nIF.. 1");
@@ -42,6 +42,31 @@ public class CobaPercabangan {
        }else {
                System.out.println("nilai variabel a lebih dari atau sama dengan 5");
         }
+        
+        System.out.println("\nIF..ELSEIF..ELSE.. 1");
+        if (a<5) {
+            System.out.println("nilai a kurang dari 5"); }
+        else if(a == 5) {
+            System.out.println("nilai a sama dengan 5"); }
+        else {
+            System.out.println("nilai a lebih dari 5"); }
+        
+        System.out.println("\nIF..ELSEIF..ELSE.. 2");
+        if (a<2) {
+            System.out.println("nilai a kurang dari 2");}
+        else if (a<4) {
+             System.out.println("nilai a kurang dari 4");}
+        else if (a<6) {
+             System.out.println("nilai a kurang dari 6");}
+        else if (a == 6) {
+             System.out.println("nilai a sama dengan 6");}
+        else {
+             System.out.println("nilai a lebih dari 6");}
+        
+    
+                
+        
+        
         
         
                     
